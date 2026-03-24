c42fb1d123ca90d1d06bcce3142a31839f7b149b
Lady Q <lady.qwickske@gmail.com>
2026-03-24 09:49:25 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-﻿// Cloudflare Worker (public, CORS-friendly)
-window.CLOUDFLARE_WORKER_URL = 'https://ccc-l1.lady-qwickske.workers.dev/';
+﻿﻿// Cloudflare Worker (public, CORS-friendly)
+window.CLOUDFLARE_WORKER_URL = 'https://ccc-league-one.lady-qwickske.workers.dev/';
 
 // Frontend should call the worker to avoid GAS CORS restrictions.
 window.GAS_WEB_APP_URL = window.CLOUDFLARE_WORKER_URL;
